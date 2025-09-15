ca05f1492a5b4f8e77a0b9451ed9ea8c30f48752
jaemina-a <tkfkd@unist.ac.kr>
2025-09-15 13:17:11 +0900

    black

--- services/department_service.py
+++ services/department_service.py
@@ -5,16 +5,18 @@ def get_all_departments():
     """모든 학과 목록 조회"""
     try:
         departments = Department.query.all()
-        
+
         result = []
         for dept in departments:
-            result.append({
-                "id": dept.id,
-                "degree_course": dept.degree_course,
-                "college": dept.college,
-                "major": dept.major,
-            })
-        
+            result.append(
+                {
+                    "id": dept.id,
+                    "degree_course": dept.degree_course,
+                    "college": dept.college,
+                    "major": dept.major,
+                }
+            )
+
         return result
 
     except Exception as e:
@@ -25,10 +27,10 @@ def get_department_by_id(department_id):
     """특정 학과 정보 조회"""
     try:
         department = Department.query.filter_by(id=department_id).first()
-        
+
         if not department:
             return None
-        
+
         return {
             "id": department.id,
             "degree_course": department.degree_course,
