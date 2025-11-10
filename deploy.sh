d1f49cbdbfee5dd97df3cf560188c263e893b13d
somin850 <somin030805@naver.com>
2025-11-10 20:51:08 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -103,5 +103,5 @@ echo "이미지 정리..."
 sudo docker image prune -f
 
 echo "=== 배포 완료! ==="
-echo "API: http://52.78.5.150:5000"
+echo "API: http://13.124.12.61:5000"
 echo "도메인: https://api.clubu.co.kr"
